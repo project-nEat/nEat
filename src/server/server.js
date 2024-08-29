9f50db5a7c0e0662ff3c4e63d63da972216e06cf
Brendan Xiong <32696639+brendanxiong@users.noreply.github.com>
2024-08-29 09:15:12 -0700

    add console log for server start up

--- src/server/server.js
+++ src/server/server.js
@@ -5,6 +5,8 @@ const app = express();
 const PORT = 3000;
 
 // serve the frontend
-app.use(express.static(path.resolve(__dirname, '../client')));
+app.use(express.static(path.resolve(__dirname, '../../dist')));
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
