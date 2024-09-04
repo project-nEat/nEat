980c877a2d43a0292c4a25d86de662094e6fe558
Brendan Xiong <32696639+brendanxiong@users.noreply.github.com>
2024-09-04 11:14:50 -0700

    work on auth page

--- src/client/components/Auth.jsx
+++ src/client/components/Auth.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Auth = () => {
   return (
-    <div className="auth flex flex-col justify-center content-center min-h-screen bg-black">
+    <div className="auth min-h-screen bg-black flex flex-col">
       <h1 className="text-white text-4xl">Login/Sign Up</h1>
-      <div className= "login-container flex flex-col justify-center">
+      <div className= "login-container flex flex-col">
         <label className="text-white" for="email">Email</label>
         <input className="text-black" type="email" placeholder="Enter Email" name="email" required />
 
